32fada2dc94557f057394173106ea3f95afdc4bd
Roman Sheludkow <roman_sheludkow@epam.com>
2018-03-13 14:58:43 +0200

    Initial co

--- Lesson1/src/EQSolver.java
+++ Lesson1/src/EQSolver.java
@@ -3,10 +3,9 @@ public class EQSolver {
         double a = 2;
         double b = -1;
         double c = -1;
-        double x1, x2;
 
-        x1 = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
-        x2 = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+        double x1 = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+        double x2 = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
 
         System.out.println(x1 + "\n" + x2);
     }

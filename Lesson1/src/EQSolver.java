21867639daa149efaec891c9d9be6d55380da9c5
Roman Sheludkow <roman_sheludkow@epam.com>
2018-03-20 15:51:58 +0200

    Lesson 2 homework

--- Lesson1/src/EQSolver.java
+++ Lesson1/src/EQSolver.java
@@ -1,8 +1,18 @@
 public class EQSolver {
+
     public static void main(String[] args) {
-        double a = 2;
-        double b = -1;
-        double c = -1;
+//        double a = 0;
+
+        // case 1
+        double a = 1;
+        double b = -8;
+        double c = 12;
+
+        //case 2
+//        double a = 5;
+//        double b = 0;
+//        double c = 30;
+
 
         double x1 = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
         double x2 = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
--- Lesson2/src/HomeWork.java
+++ Lesson2/src/HomeWork.java
@@ -1,20 +1,92 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class HomeWork {
 
 
     public static void main(String[] args) {
-        printAsterisks(7);
-        for (int i = 0; i < 5; i++) {
-            int[] randomArray = new Random().ints(10, 1, 10).toArray();
-            System.out.println("-----------Unsorted---------------");
-            printArray(randomArray);
-            bubbleSort(randomArray);
-            System.out.println("-----------Sorted-----------------");
-            printArray(randomArray);
-            int searchable = (int) (9 * Math.random() + 1);
-            System.out.println(isInArray(randomArray, searchable) + " searchable - " + searchable);
-            System.out.println();
+        Scanner scan = new Scanner(System.in);
+
+
+        // Task # 1
+//        System.out.println("Please input number from 2 to 9: ");
+//        int stepsAsterisks = scan.nextInt();
+//        printAsterisks(stepsAsterisks);
+//
+//
+        //Task 2 & Task 4
+//        System.out.println("Please input amount of loops to test array sort and binary search: ");
+//        int loops = scan.nextInt();
+//        for (int i = 1; i <= loops; i++) { //Tasks # 2, 3
+//            int[] randomArray = new Random().ints(10, 1, 10).toArray();
+//            System.out.println("-----------Unsorted---------------");
+//            printArray(randomArray);
+//            bubbleSort(randomArray);
+//            System.out.println("-----------Sorted-----------------");
+//            printArray(randomArray);
+//            int searchable = (int) (9 * Math.random() + 1);
+//            System.out.println("Is searchable " + searchable + " is in array - " + isInArray(randomArray, searchable));
+//            System.out.println();
+//        }
+
+        //Task 5
+//        System.out.println("Please input to determine whether it even or odd: ");
+//        int number = scan.nextInt();
+//        printOddOrEven(number);
+
+
+        //Task 6
+//        System.out.println("Please input number from 1 to 9 to have its string representation: ");
+//        int number = scan.nextInt();
+//        printStringOfNumber(number);
+
+
+        //Task 7
+//        printSumOfHundred();
+
+
+        //Task 6(2)
+        double customPI = calculateCustomPI();
+        System.out.println(customPI + " " + Math.PI);
+    }
+
+    private static double calculateCustomPI() {
+        double Pi = 4;
+        boolean plus = false;
+        for (int i = 3; i < 10000000; i += 2) {
+            if (plus) {
+                Pi += 4.0 / i;
+            } else {
+                Pi -= 4.0 / i;
+            }
+            plus = !plus;
+        }
+        return Pi;
+    }
+
+    private static void printSumOfHundred() {
+        int result = 0;
+        for (int i = 1; i <= 100; i++) {
+            result += i;
+        }
+        System.out.println("Sum is - " + result + "\nAverage is - " + (double) result / 100);
+    }
+
+    private static void printStringOfNumber(int number) {
+        if (number > 0 && number < 10) {
+            String[] values = {"One", "Two", "Three", "Four", "Fife", "Six", "Seven", "Eight", "Nine"};
+            System.out.println(values[number - 1]);
+        } else {
+            System.out.println("Number is out of range");
+        }
+
+    }
+
+    private static void printOddOrEven(int value) {
+        if (value % 2 == 0) {
+            System.out.println(value + " - Even number");
+        } else {
+            System.out.println(value + " - Odd number");
         }
     }
 
@@ -31,8 +103,11 @@ public class HomeWork {
     }
 
     private static void printArray(int[] array) {
+        if (array.length > 1) {
+            System.out.print("|");
+        }
         for (int elem : array) {
-            System.out.print(elem + " ");
+            System.out.print(elem + "|");
         }
         System.out.println("");
     }
